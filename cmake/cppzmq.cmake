384fb4c74e6218d7c2996828a1d93d6e5ea1af58
Jonathan Haws <jonathan.haws@witricity.com>
2021-10-30 14:50:55 -0600

    Disable CPPZMQ tests

--- cmake/cppzmq.cmake
+++ cmake/cppzmq.cmake
@@ -1,5 +1,5 @@
-message(INFO " -> Fetching cppzmq...")
 include(FetchContent)
+set(CPPZMQ_BUILD_TESTS OFF CACHE INTERNAL "") # Disable CPPZMQ tests
 FetchContent_Declare(cppzmq_git
   GIT_REPOSITORY      https://github.com/zeromq/cppzmq.git
   GIT_TAG             v4.8.1
